da38b93ddd7fc257e4fcaad1d364ccb249e8688d
pindeck <136303386+pindeck@users.noreply.github.com>
2025-05-15 13:48:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("🎈 tukang balon")
-st.image("amalak/images.jpeg", width 200)
+st.image("amalak/images.jpeg", width=200)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
