efa9bd704176ffd8ef37f6a9f426a09b781c95b6
pindeck <136303386+pindeck@users.noreply.github.com>
2025-05-15 14:31:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("HIDUP JOKOWI")
+st.image("9502196f753b68d5c33937de127ee405.jpg", width=200)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
